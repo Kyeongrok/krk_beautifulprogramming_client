622acd1195f4293c8bb224130a526b49ce490072
mattheu <script-deployer@me.com>
2018-07-31 18:09:08 +0900

    click event box추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from './components/Box';
+import axios from 'axios'
 
 class App extends Component {
+  componentWillMount(){
+    console.log("hello");
+    axios.get({
+      url:'https://k3q0fbhd62.execute-api.ap-northeast-2.amazonaws.com/prod/greeting?name=kyeongrok',
+      headers:{
+        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true
+      }
+    })
+      .then(function (response) {
+        console.log('here');
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log('error');
+        console.log(error);
+      });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Box/>
       </div>
     );
   }
---
+++ src/components/Box.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Box extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleOnClick = this.handleOnClick
+  }
+  handleOnClick(){
+    alert("hello")
+  }
+  render() {
+    return (
+      <div>
+        지갑주소:<input type="text" />
+        <button onClick={()=>this.handleOnClick()}>받기</button>
+      </div>
+    )
+  }
+}
+
+export default Box
